3dafbbfd60f63772f78eb5e433eb83ea7e0028d1
OffTheWaller <1031118178@qq.com>
2018-12-08 11:57:08 +0800

    login页面背景解决

---
+++ src/views/login/Login.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="login">
+    这是登录页
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Login'
+}
+</script>
+
+<style lang="scss">
+    .login {
+        width: 100%;
+        height: 100%;
+        background: url('~@/assets/images/login-bg.png') 100% 100% no-repeat;
+    }
+</style>
