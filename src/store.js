0c21b224d81a0919317840ba88cbf62ff0020993
OffTheWaller <1031118178@qq.com>
2018-12-11 00:06:48 +0800

    首页header

--- src/store.js
+++ src/store.js
@@ -5,7 +5,46 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
-
+    nav: [
+      {
+        title: '首页',
+        path: '/home/homeSystem',
+        child: [
+          {
+            sub: '系统首页',
+            menu: [
+              {
+                name: '系统首页',
+                path: '/home/homeSystem'
+              },
+              {
+                name: '账户设置',
+                path: '/home/accountSetting'
+              }
+            ]
+          }
+        ]
+      },
+      {
+        title: '商品',
+        path: '/goods/goodsManagement',
+        child: [
+          {
+            sub: '系统首页',
+            menu: [
+              {
+                name: '系统首页',
+                path: '/home/homeSystem'
+              },
+              {
+                name: '账户设置',
+                path: '/home/accountSetting'
+              }
+            ]
+          }
+        ]
+      }
+    ]
   },
   mutations: {
 
--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -1,12 +1,41 @@
 <template>
   <div class="home">
-    这是项目的首页
+    <el-container>
+      <el-header class="nav" height="90px">
+        <common-header></common-header>
+      </el-header>
+      <el-container class="main-content">
+        <el-aside class="sidebar">这是侧边栏</el-aside>
+        <el-main>这是主内容区</el-main>
+      </el-container>
+    </el-container>
   </div>
 </template>
 
 <script>
+import CommonHeader from '@/components/CommonHeader'
 
 export default {
-  name: 'Home'
+  name: 'Home',
+  components: {
+    CommonHeader
+  }
 }
 </script>
+
+<style lang="scss">
+  .el-header.nav {
+    width: 100%;
+    height: 90px;
+    background: #5bc0bf;
+  }
+  .main-content {
+    width: 1634px;
+    background: #ccc;
+    margin: 0 auto;
+  }
+  .sidebar {
+    width: 260px;
+    background: yellow;
+  }
+</style>
