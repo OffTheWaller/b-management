77bee4d1e8f1e8a0284d3850e5bdb44deb100e66
OffTheWaller <1031118178@qq.com>
2018-12-21 00:00:49 +0800

    完成系统日志

--- src/views/home/LoginLog.vue
+++ src/views/home/LoginLog.vue
@@ -1,13 +1,107 @@
 <template>
-    <div>这是登录日志页面</div>
+    <div>
+        <!-- 内容标题区域 -->
+        <div class="main-title">
+            <div class="greenbar fl"></div>
+            <h5 class="fl">登录日志</h5>
+            <button class="flush fr"><span class="iconfont">&#xe654;</span>  刷新</button>
+        </div>
+        <div class="main-content">
+            <!-- 数据列表 -->
+            <div class="table-title">
+                数据列表
+            </div>
+            <el-table :data="tableData" style="width: 100%" border>
+              <el-table-column label="时间" prop="time"></el-table-column>
+              <el-table-column label="IP" prop="ip"></el-table-column>
+              <el-table-column label="地区" prop="site"></el-table-column>
+              <el-table-column label="浏览器" prop="browser"></el-table-column>
+            </el-table>
+        </div>
+    </div>
 </template>
 
 <script>
 export default {
-    name: 'LoginLog'
+    name: 'LoginLog',
+    data () {
+      return {
+        tableData: [{
+          time: '2017-07-03 14:36:21',
+          ip: '183.14.15.113',
+          site: '广东省深圳市',
+          browser: 'Firefox 45'
+        },
+        {
+          time: '2017-07-03 14:36:21',
+          ip: '183.14.15.113',
+          site: '广东省深圳市',
+          browser: 'Firefox 45'
+        },
+        {
+          time: '2017-07-03 14:36:21',
+          ip: '183.14.15.113',
+          site: '广东省深圳市',
+          browser: 'Firefox 45'
+        },
+        {
+          time: '2017-07-03 14:36:21',
+          ip: '183.14.15.113',
+          site: '广东省深圳市',
+          browser: 'Firefox 45'
+        },
+        {
+          time: '2017-07-03 14:36:21',
+          ip: '183.14.15.113',
+          site: '广东省深圳市',
+          browser: 'Firefox 45'
+        }]
+      }
+    }
 }
 </script>
 
-<style lang="scss">
-    
+<style lang="scss" scoped>
+@import '~@/assets/styles/variables.scss';
+    .main-title {
+        height: 60px;
+        background: $side-bar;
+        .greenbar {
+            width: 14px;
+            background: $theme-color;
+            height: 28px;
+            margin-top: 16px;
+        }
+        h5 {
+            line-height: 60px;
+            padding-left: 30px;
+            font-size: 22px;
+            color: #595757;
+        }
+        .flush {
+            width: 90px;
+            height: 42px;
+            margin-top: 8px;
+            margin-right: 42px;
+            font-size: 16px;
+            color: #898989;
+            background: #f4f5f5;
+            border: 1px solid #cbcbcb;
+            border-radius: 10px;
+            cursor: pointer;
+            outline: none;
+        }
+    }    
+    .main-content {
+        width: 100%;
+        padding: 34px;
+        
+    }
+    .main-content /deep/ th {
+          background: #F7F8F8;
+          text-align: center;
+          font-weight: 700;
+          color: #595757;
+          border: 1px solid #F7F8F8;
+        }
 </style>
--- src/views/home/SystemInfo.vue
+++ src/views/home/SystemInfo.vue
@@ -6,6 +6,57 @@
             <h5 class="fl">系统信息</h5>
             <button class="flush fr"><span class="iconfont">&#xe654;</span>  刷新</button>
         </div>
+        <!-- 系统信息表格 -->
+        <table class="info-table">
+            <tr>
+                <td class="bg-grey">服务器操作系统</td>
+                <td>Linux(114.55.34.251）</td>
+                <td class="bg-grey">Web服务器</td>
+                <td>nginx/1.10.2</td>
+            </tr>
+            <tr>
+                <td class="bg-grey">PHP版本</td>
+                <td>5.6.30</td>
+                <td class="bg-grey">MySQL版本</td>
+                <td>5.5.56</td>
+            </tr>
+            <tr>
+                <td class="bg-grey">安全模式</td>
+                <td>否</td>
+                <td class="bg-grey">安全模式GID</td>
+                <td>否</td>
+            </tr>
+            <tr>
+                <td class="bg-grey">Socket支持</td>
+                <td>是</td>
+                <td class="bg-grey">时区设置</td>
+                <td>Asia/Shanghai</td>
+            </tr>
+            <tr>
+                <td class="bg-grey">GD版本</td>
+                <td>GD2(JPEG GIF PNG)</td>
+                <td class="bg-grey">Zlib支持</td>
+                <td>是</td>
+            </tr>
+            <tr>
+                <td class="bg-grey">IP库版本</td>
+                <td>20071024</td>
+                <td class="bg-grey">文件上传的最大大小</td>
+                <td>40M</td>
+            </tr>
+            <tr>
+                <td class="bg-grey">程序版本</td>
+                <td>v2.2.2 RELEASE 20170629</td>
+                <td class="bg-grey">安全日期</td>
+                <td>2017-05-02</td>
+            </tr>
+            <tr>
+                <td class="bg-grey">编码</td>
+                <td>UTF-8</td>
+                <td class="bg-grey"></td>
+                <td></td>
+            </tr>
+        </table>
     </div>
 </template>
 
@@ -44,6 +95,25 @@ export default {
             border-radius: 10px;
             cursor: pointer;
             outline: none;
+        }
+    }
+    // 表格部分
+    .info-table {
+        width: 1295px;
+        height: 409px;
+        margin: 36px auto;
+        border-right: 1px solid #B5B5B5;
+        border-bottom: 1px solid #B5B5B5;
+        td {
+            line-height: 52px;
+            border-top: 1px solid #B5B5B5;
+            border-left: 1px solid #B5B5B5;
+            padding-left: 42px;
+            font-size: 16px;
+            color: #595757;
+        }
+        .bg-grey {
+            background: #EFEFEF;
         }
     }
 </style>
